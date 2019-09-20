b6b8adc64c04f728e981c34def942f6ba4741093
Soeren Wegener <wegener92@gmail.com>
2019-09-20 02:36:22 +0200

    Add table and first hackumenta data

--- src/sections/events.py
+++ src/sections/events.py
@@ -1,8 +1,42 @@
+import re
+from pprint import pprint
+from typing import List, Dict, Union
+
 from flask import Blueprint, render_template
+from pathlib import Path
+import yaml
 
 bp = Blueprint('events', __name__, url_prefix='/events', template_folder='templates')
 
 
-@bp.route('/index')
+def create_cost_table(data, title=None, depth=0) -> List[Dict[str, Union[str, float, bool]]]:
+    result = []
+    for element in data:
+        description = ' – '.join(filter(None, [title, element['title']]))
+        if 'items' in element:
+            sub_elements = create_cost_table(element['items'], description, depth+1)
+            result += sub_elements
+        else:
+            result.append({'description': description, 'costs': element['costs'], 'depth': depth})
+    description = ' – '.join(filter(None, [title, 'SUMME']))
+    sub_sum = sum([x['costs'] for x in result if not x.get('is_sum')])
+    result.append({'description': description, 'costs': sub_sum, 'is_sum': True, 'depth': depth})
+    return result
+
+
+@bp.route('/')
 def index():
-    return render_template('events/index.html')
+    events_path = Path('data/events/')
+    paths = events_path.glob('**/*.yml')
+    event_list = [re.search(r'(\d+)/(.+).yml', str(p)).groups() for p in paths]
+    return render_template('events/index.html', event_list=event_list)
+
+
+@bp.route('/<string:year>/<string:event_name>')
+def event(year, event_name):
+    with open(Path('data/events') / year / f'{event_name}.yml') as f:
+        event_data = yaml.safe_load(f)
+    pprint(event_data)
+    expenditures = create_cost_table(event_data['expenditures'])
+    total_costs = sum([x['costs'] for x in expenditures if not x.get('is_sum')])
+    return render_template('events/detail.html', event=event_data, expenditures=expenditures, total_costs=total_costs)
