8079d7ebde0ade4bf81abe52bf3930dfd6eff837
Soeren Wegener <wegener92@gmail.com>
2020-02-19 01:03:58 +0100

    Break down costs

--- src/sections/projects.py
+++ src/sections/projects.py
@@ -9,4 +9,7 @@ bp = Blueprint('projekte', __name__, url_prefix='/projekte', template_folder='te
 def index():
     with open(Path('data/projects.yml')) as f:
         data = yaml.safe_load(f)
+    for year, projects in data.items():
+        for project in projects:
+            project['total_costs'] = sum([x['price'] * x.get('quantity', 1) for x in project['costs']])
     return render_template('projects/index.html', data=data)
