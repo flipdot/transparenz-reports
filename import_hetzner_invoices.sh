2d3dfc870ac16444177530357412a1d89d6e04ce
Soeren Wegener <wegener92@gmail.com>
2022-07-02 21:02:24 +0200

    Add new invoices

--- import_hetzner_invoices.sh
+++ import_hetzner_invoices.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-pipenv shell
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 OUTPUT_PATH="$SCRIPT_PATH/data/running_expenses/hcloud"
 
@@ -13,6 +12,6 @@ for csv_path in $1/Hetzner_*.csv; do
     timestamp=$(echo $csv_path | grep -Po "(?<=Hetzner_)\d{4}-\d{2}-\d{2}");
     pdf_path="${csv_path::-3}pdf"
     echo "Converting $csv_path..."
-    hetzner-fix-report $csv_path $pdf_path -o "$OUTPUT_PATH/$timestamp.csv"
+    poetry run hetzner-fix-report $csv_path $pdf_path -o "$OUTPUT_PATH/$timestamp.csv"
 done
 
